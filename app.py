b18892a19fc3fd7d02188211d5ede050abc6a18d
Innocent Onwukanjo <innocent.onwukanjo@mtn.com>
2024-06-30 18:40:37 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    data = request.get_json()
+    # Process the data here (if needed)
+    return jsonify({"message": "Data received successfully", "data": data}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
